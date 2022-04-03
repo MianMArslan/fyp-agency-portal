d4a679204f92afa3fac6c82606159d1ccb8a64c0
idevnerds-Arslan <arslan@idevnerds.com>
2022-04-03 15:54:35 +0500

    add httpClient update package.josn to run on port 3001

---
+++ src/services/httpClient.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+const httpClient = axios.create({
+  baseURL: `http://localhost:4001/`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+const httpFileUpload = axios.create({
+  baseURL: `http://localhost:4001/`,
+  headers: {
+    "content-type": "multipart/form-data",
+  },
+  withCredentials: true,
+});
+
+export async function GET(url, params) {
+  try {
+    const response = await httpClient.get(url, params);
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function POST(url, params) {
+  try {
+    const response = await httpClient.post(url, params);
+    return response.data;
+  } catch (error) {
+    return error?.response;
+  }
+}
+
+export async function DELETE(url, params) {
+  try {
+    const response = await httpClient.delete(url, params);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function UPDATE(url, params) {
+  try {
+    const response = await httpClient.put(url, params);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function UPLOADFILE(url, params) {
+  try {
+    const response = await httpFileUpload.post(url, params);
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
